5d949f783106c33c3c6e7656ca3ea0dc4c884886
chuican <2940058482@qq.com>
2020-08-10 00:54:36 +0800

    add hireadis

---
+++ vote-manager/build/CMakeFiles/vote.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/vote.dir/vote.cpp.o"
+  "vote.pdb"
+  "vote"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/vote.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
